5632977a20519540666849200baf553b1ed7f737
Liorshein <liorsheinwork@gmail.com>
2022-08-30 04:15:40 +0300

    Added simple server routes &  controllers

--- server/server.ts
+++ server/server.ts
@@ -1,14 +1,14 @@
 import express, { Express } from 'express';
 import cors from 'cors';
 import { json } from 'body-parser';
-import registerRouter from './routes/register';
+import authRouter from './routes/authentication';
 import { initDb } from './db';
 
 const app: Express = express();
 app.use(cors());
 app.use(json());
 
-app.use('/', registerRouter)
+app.use('/', authRouter)
 
 initDb()
 
