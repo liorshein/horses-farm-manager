b20ca221d23ebc49cbbbc02af7570e3c17d402b7
Liorshein <liorsheinworl@gmail.com>
2022-09-01 23:28:43 +0300

    Changed css to modules

--- client/src/components/LoginComp.tsx
+++ client/src/components/LoginComp.tsx
@@ -1,4 +1,5 @@
 import { useAuth } from './AuthProvider';
+import styles from "../pages/Login/loginComp.module.scss"
 
 type Props = {
   switchPage: (a: string) => void
@@ -12,20 +13,20 @@ const Login = (props: Props) => {
 
   return (
     <>
-      <h1 className='header'>Welcome!</h1>
-      <form>
-        <div className="form-group">
+      <h1 className={styles.header}>Welcome!</h1>
+      <form autoComplete="off">
+        <div className={styles.form_group}>
           <label>Username</label>
           <input type="text" name="username" id="username" placeholder='Username'
             value={loginValues.username} onChange={onChange} />
         </div>
-        <div className="form-group">
+        <div className={styles.form_group}>
           <label>Password</label>
           <input type="password" name="password" id="password" placeholder='Password'
             value={loginValues.password} onChange={onChange} />
         </div>
-        <button className='login-btn' onClick={onLogin}>Login</button>
-        <button className='register-btn' type="button" onClick={() => props.switchPage("Register")}>Not a member? Sign In!</button>
+        <button className={styles.login_btn} onClick={onLogin}>Login</button>
+        <button className={styles.register_btn} type="button" onClick={() => props.switchPage("Register")}>Not a member? Sign In!</button>
       </form>
     </>
   )
