a8e143b1cba6af64a2e27ecb918f87af692a2cca
Liorshein <liorsheinworl@gmail.com>
2022-09-01 10:33:00 +0300

    Added style for login page

--- client/src/components/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Navigate, useLocation } from 'react-router-dom';
 import { useAuth } from './AuthProvider'
 
@@ -14,7 +13,7 @@ const ProtectedRoute = (props: Props) => {
     const { token } = appContext    
     
     if (!token) {
-        return <Navigate to="/home" replace state={{ from: location }} />;
+        return <Navigate to="/login" replace state={{ from: location }} />;
     }
 
     return props.children
