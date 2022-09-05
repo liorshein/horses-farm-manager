563cbd227ff8f5aadbcccec6904c6d3aed889eb7
Liorshein <liorsheinworl@gmail.com>
2022-09-06 01:45:12 +0300

    Refactor lesson hours and db

--- client/src/components/SearchTime.tsx
+++ client/src/components/SearchTime.tsx
@@ -3,7 +3,9 @@ import React, { useEffect, useState } from 'react'
 import DatePicker from 'react-datepicker'
 import UserService from '../services/userService'
 
-type Props = {}
+type Props = {
+    setAvailableHours: (a: string[]) => void
+}
 
 type Horse = {
     horse_id: number
@@ -16,7 +18,7 @@ type Horse = {
 const SearchTime = (props: Props) => {
     const [day, setDay] = useState(new Date())
     const [horseInfo, setHorseInfo] = useState<Horse[]>([])
-    const [selectedHorse, setSelectedHorse] = useState('')
+    const [selectedHorseId, setSelectedHorseId] = useState('')
 
     useEffect(() => {
         const getData = async () => {
@@ -26,13 +28,15 @@ const SearchTime = (props: Props) => {
         getData()
     }, [])
 
-    useEffect(() => {
-        const getData = async () => {
-            const availableHours = await UserService.getWorkHours(day)
-            
+    const handleClick = async () => {        
+        if (selectedHorseId && day !== undefined && selectedHorseId !== "Pick Horse"){
+            let dateFormat = day.toISOString().split("T")[0] + " 00:00:00+00"            
+            const availableHours = await (await UserService.getLessons(selectedHorseId, dateFormat)).data.filteredResults
+            props.setAvailableHours(availableHours);
+        } else {
+            alert("Please select horse and date!")
         }
-        getData()
-    }, [])
+    }
 
     return (
         <>
@@ -44,13 +48,15 @@ const SearchTime = (props: Props) => {
             />
 
             <div className="content">
-                <select value={selectedHorse} onChange={(e) => setSelectedHorse(e.target.value)}>
+                <select value={selectedHorseId} onChange={(e) => setSelectedHorseId(e.target.value)}>
                     <option>Pick Horse</option>
                     {horseInfo.map((horse: Horse) => {
                         return <option key={horse.horse_id} value={horse.horse_id}>{horse.name}</option>
                     }
                     )}</select>
             </div>
+
+            <button onClick={handleClick}>Check Available Hours</button>
         </>
     )
 }
--- server/controllers/users.ts
+++ server/controllers/users.ts
@@ -2,6 +2,7 @@ import { RequestHandler } from "express";
 import { client } from "../db"
 import Cookies from 'universal-cookie';
 import jwt from 'jsonwebtoken'
+import { filterHours } from './helpFunctions'
 
 export const authenticateToken: RequestHandler = (req: any, res, next) => {
     const token = new Cookies(req.headers.cookie).get('token');
@@ -47,27 +48,32 @@ export const addLesson: RequestHandler = async (req: any, res) => {
 
 export const getLessons: RequestHandler = async (req: any, res) => {
     const InstructorId = (req.user)._id
+    const horseId = req.query.horse_id
+    const date = req.query.date
 
-    const result = await client.query(
-        `SELECT * FROM lessons WHERE instructor_id=$1`, [
-        InstructorId
-    ]);
+    const result = (await client.query(
+        `SELECT * FROM lessons WHERE instructor_id=$1 AND horse_id=$2 AND date=$3`, [
+        InstructorId,
+        horseId,
+        date
+    ])).rows;
 
-    res.send({ result });
+    const filteredResults = filterHours(result)
+
+    res.send({ filteredResults });
 }
 
 export const getHorsesHours: RequestHandler = async (req, res) => {
-    // const horseId = req.query.horse_id
-    // console.log(horseId);
-    const result = (await client.query('SELECT * FROM horses_lessons')).rows
-    // const result = (await client.query('SELECT * FROM horses_lessons WHERE horse_id=$1', [horseId]))
-
+    const horseId = req.query.horse_id
+    const date = req.query.date
+    const result = (await client.query(`SELECT * FROM horses_lessons WHERE horse_id=$1 AND date=$2`, [horseId, date]))
     res.send({ result });
 }
 
 export const getInstructorHours: RequestHandler = async (req: any, res) => {
     const InstructorId = (req.user)._id
-    const result = (await client.query('SELECT * FROM instructor_lessons WHERE instructor_id=$1', [InstructorId])).rows
+    const date = req.query.date
+    const result = (await client.query(`SELECT * FROM instructor_lessons WHERE instructor_id=$1 AND date=$2`, [InstructorId, date]))
     res.send({ result });
 }
 
@@ -94,14 +100,6 @@ export const addHorse: RequestHandler = async (req: any, res) => {
         req.body.breed,
         req.body.assignable,
     ]);
-
-    const result2 = await client.query(
-        `INSERT INTO horses_lessons(date)
-        SELECT generate_series(now(),now() + '1 years','1 day'::interval)`);
-
-    const horseId = result1.rows[0].horse_id
-
-    const result3 = await client.query(`UPDATE horses_lessons SET horse_id = $1 WHERE horse_id is null`, [horseId]);
 
     res.send({ success: true });
 }
