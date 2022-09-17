29761be06103589a04cf6d72f5a0031e34fc170a
Liorshein <liorsheinworl@gmail.com>
2022-09-17 17:34:47 +0300

    commit

--- client/src/services/userService.ts
+++ client/src/services/userService.ts
@@ -3,38 +3,34 @@ import { Student } from "../pages/Students/Students";
 
 axios.defaults.withCredentials = true
 
-const PORT = process.env.PORT
-
-const API_URL = PORT + "/";
-
 //* Put functions
 
 const updateArrived = (lesson_id: string, boolean: string) => {
     let params = new URLSearchParams({ lesson_id: lesson_id, arrived: boolean})
-    axios.put(API_URL + `update-arrived?${params}`)
+    axios.put(`/api/update-arrived?${params}`)
 }
 
 //* Delete functions
 
 const deleteStudent = (student_id: string) => {
     let params = new URLSearchParams({ student_id: student_id })
-    axios.delete(API_URL + `delete-student?${params}`)
+    axios.delete(`/api/delete-student?${params}`)
 }
 
 const deleteHorse = (horse_id: string) => {
     let params = new URLSearchParams({ horse_id: horse_id })
-    axios.delete(API_URL + `delete-horse?${params}`)
+    axios.delete(`/api/delete-horse?${params}`)
 }
 
 const deleteLesson = (lesson_id: string) => {
     let params = new URLSearchParams({ lesson_id: lesson_id })
-    axios.delete(API_URL + `delete-lesson?${params}`)
+    axios.delete(`/api/delete-lesson?${params}`)
 }
 
 //* Post functions
 
 const addHorse = async (name: string, age: number, breed: string, assignable: boolean) => {
-    const result = await axios.post(API_URL + "add-horse", {
+    const result = await axios.post("/api/add-horse", {
         name: name,
         age: age,
         breed: breed,
@@ -45,7 +41,7 @@ const addHorse = async (name: string, age: number, breed: string, assignable: bo
 };
 
 const addStudent = (inputs: Student) => {
-    axios.post(API_URL + "add-student", {
+    axios.post("/api/add-student", {
         name: inputs.student_name,
         id: inputs.id,
         date_of_birth: inputs.date_of_birth,
@@ -60,7 +56,7 @@ const addStudent = (inputs: Student) => {
 };
 
 const addLesson = (horse_id: number, date: string, lesson_time: string, student_id: number) => {
-    axios.post(API_URL + "add-lesson", {
+    axios.post("/api/add-lesson", {
         horse_id: horse_id,
         date: date,
         lesson_time: lesson_time,
@@ -71,51 +67,51 @@ const addLesson = (horse_id: number, date: string, lesson_time: string, student_
 //* Get functions
 
 const getPersonalInfo = async () => {
-    return (await axios.get(API_URL + "user")).data.result
+    return (await axios.get("/api/user")).data.result
 }
 
 const getFavoriteHouse = async () => {
-    let result = await axios.get(API_URL + `favorite-horse`);
+    let result = await axios.get(`/api/favorite-horse`);
     return result
 };
 
 const getSalaryPerMonth = async (date: string) => {
     let params = new URLSearchParams({ year_month_str: date })    
-    let results = await (await axios.get(API_URL + `salary?${params}`)).data.result.rows[0].count
+    let results = await (await axios.get(`/api/salary?${params}`)).data.result.rows[0].count
     return results
 }
 
 const getLessonsPerMonth = async () => {
-    let results = await (await axios.get(API_URL + `lessons-per-month`)).data.result.rows
+    let results = await (await axios.get(`/api/lessons-per-month`)).data.result.rows
     return results
 }
 
 const getMonthOfLessons = async () => {
-    let results = await (await axios.get(API_URL + `lessons-monthly`)).data.result.rows  
+    let results = await (await axios.get(`/api/lessons-monthly`)).data.result.rows  
     return results
 }
 
 const getAvailableHours = async (horse_id: string, date: string) => {
     let params = new URLSearchParams({ horse_id: horse_id, date: date })
-    let availableHours = await axios.get(API_URL + `lessons-available?${params}`);
+    let availableHours = await axios.get(`/api/lessons-available?${params}`);
     return availableHours
 };
 
 const getUserStudentsInfo = async () => {
-    return (await axios.get(API_URL + "user-students")).data.result
+    return (await axios.get("/api/user-students")).data.result
 }
 
 const getHorsesInfo = async () => {
-    return (await axios.get(API_URL + "horses")).data.result
+    return (await axios.get("/api/horses")).data.result
 }
 
 const getAvailableHorses = async () => {
-    return (await axios.get(API_URL + "horses-available")).data.result
+    return (await axios.get("/api/horses-available")).data.result
 }
 
 const getUserLessons = async (date: string) => {
     let params = new URLSearchParams({ date: date })
-    return (await axios.get(API_URL + `lessons?${params}`)).data.result
+    return (await axios.get(`/api/lessons?${params}`)).data.result
 }
 
 const UserService = {
