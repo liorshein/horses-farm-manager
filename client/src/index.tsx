fa46471bd340d7877946f8b37169661949c7fbb5
Liorshein <liorsheinwork@gmail.com>
2022-09-30 22:25:45 +0300

    Added disable react-dev-tools in production

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevTools()
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <App />
